8b1134cfb10017f9db29a0abe197c4b504e4d055
Tina Tae <v-ktae@microsoft.com>
2021-03-16 07:01:33 -0700

    Add Player class following Evgeniya UML

---
+++ WarGame/Player.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WarGame
+{
+    public class Player
+    {
+        private string playerName;
+        public Queue<Card> playerCards;
+        private List<Card> cardsForShuffle;
+
+        public Player(string player)
+        {
+            playerName = player;
+            playerCards = new Queue<Card>();
+            cardsForShuffle = new List<Card>();
+        }
+
+        public Card ShowCard()
+        {
+            return playerCards.Dequeue();
+        }
+
+        public void AddCards(List<Card> newCards)
+        {
+            //cardsForShuffle = cardsForShuffle.Concat(newCards).ToList();  // TRIED THIS. WHY DOESN'T WORK
+            newCards.ForEach(card => cardsForShuffle.Add(card));
+        }
+
+        public void MoveToPlayerCards()
+        {
+            playerCards = Service.Shuffle(cardsForShuffle);
+        }
+    }
+}
