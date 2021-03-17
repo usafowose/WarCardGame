0a718cd5f3ec619cca4251d2fc9c6c18b3acb4e0
Andrew Fowose (Aerotek US) <v-afowose@microsoft.com>
2021-03-17 17:47:45 -0400

    Adding Services class and shuffling deck in constructor

--- WarGame_ClassLib/Card1.cs
+++ WarGame_ClassLib/Card1.cs
@@ -20,31 +20,6 @@ namespace WarGame_ClassLib
         }
     }
 
-    //    class CheckCard
-    //    {
-    //        static void Deck()
-    //        {
-    //            var Deck = new List<Card>() { };
-
-
-    //            List<int> NumValues = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
-    //            List<string> SuitNames = new List<string>() { "Spades, Hearts, Diamonds, Clubs" };
-    //            int DeckSize = 52;
-    //            List<Card> CardsInDeck = new List<Card>(); 
-
-    //            for (int i = 0; i < Numbers.Count; i++)
-    //            {
-    //                for (int j = 0; j < Suites.Count; j++)
-    //                {
-    //                    string name = i == 0 ? $"AceOf{Suites[i]}" : $"{Numbers[i]}Of{Suites[j]}";
-
-    //                    Card card = new Card(name, Numbers[i]);
-    //                    Deck.Add(card);
-    //                }
-    //            }
-
-
-    //        }
-    //    }
+   
 }
 
