879685fc80b9f70970cb6d5909a73758a63e22ff
Andrew Fowose (Aerotek US) <v-afowose@microsoft.com>
2021-03-17 15:04:14 -0400

    Removed Testconsole proj and commented Card1

--- WarGame_ClassLib/Card1.cs
+++ WarGame_ClassLib/Card1.cs
@@ -20,29 +20,29 @@ namespace WarGame_ClassLib
         }
     }
 
-    //    class CheckCard 
-    //    {
-    //        static void Deck() 
-    //        {
-    //            var Deck = new List<Card>(){ }; 
+//    class CheckCard
+//    {
+//        static void Deck()
+//        {
+//            var Deck = new List<Card>() { };
 
 
-    //            List<int> Numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
-    //            List<string> Suites = new List<string>() { "Spades, Hearts, Diamonds, Clubs" };
+//            List<int> Numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+//            List<string> Suites = new List<string>() { "Spades, Hearts, Diamonds, Clubs" };
 
-    //            for (int i = 0; i < Numbers.Count; i++)
-    //            {
-    //                for (int j = 0; j < Suites.Count; j++)
-    //                {
-    //                    string name = i == 0 ? $"AceOf{Suites[i]}":  $"{Numbers[i]}Of{Suites[j]}"; 
+//            for (int i = 0; i < Numbers.Count; i++)
+//            {
+//                for (int j = 0; j < Suites.Count; j++)
+//                {
+//                    string name = i == 0 ? $"AceOf{Suites[i]}" : $"{Numbers[i]}Of{Suites[j]}";
 
-    //                    Card card = new Card(name, Numbers[i]);
-    //                    Deck.Add(card); 
-    //                }
-    //            }
+//                    Card card = new Card(name, Numbers[i]);
+//                    Deck.Add(card);
+//                }
+//            }
 
 
-    //        }
-    //    }
-}
+//        }
+//    }
+//}
 
