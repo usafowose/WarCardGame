8fed352d7d20477303ba51355a10d947cd814d7b
Evgeniya Chernaya <v-echernaya@microsoft.com>
2021-03-16 19:03:23 -0700

    implement all main methods in Game class

--- WarGame.Tests/GameTests.cs
+++ WarGame.Tests/GameTests.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace WarGame.Tests
 {
     [TestClass]
-    public class UnitTest1
+    public class GameTests
     {
         [TestMethod]
         public void TestMethod1()
