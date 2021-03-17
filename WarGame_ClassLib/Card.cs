5f108acde265fb4ce0e32673ad500fd70073bf0d
Tina Tae <v-ktae@microsoft.com>
2021-03-16 23:24:19 -0700

    Add Player class & Player tests. Made temp changes to Card class

--- WarGame_ClassLib/Card.cs
+++ WarGame_ClassLib/Card.cs
@@ -6,15 +6,33 @@ using System.Threading.Tasks;
 
 namespace WarGame_ClassLib
 {
-    class Card
-    {
-        bool IsShowing;
-        Dictionary<int, string> NameAndValue; 
+    //class Card
+    //{
+    //    bool IsShowing;
+    //    Dictionary<int, string> NameAndValue;
 
-        public Card(Dictionary<int, string> CardType) 
+    //    public Card(Dictionary<int, string> CardType)
+    //    {
+    //        IsShowing = false;
+    //        NameAndValue = CardType;
+    //    }
+    //}
+
+    //Note: sorry if this is confusing.
+    //This was based on Evgeniya chat update & I made so Player tests would work?
+    //Can comment-out but please don't delete.
+    //This goes with Player tests for now. Thank you!
+    public class Card
+    {
+        public bool IsShow { get; set; }
+        public int Value { get; set; }
+        public string Name { get; set; }
+
+        public Card(int num, string suit)
         {
-            IsShowing = false;
-            NameAndValue = CardType; 
+            IsShow = false;
+            Value = num;
+            Name = suit;
         }
     }
 }
